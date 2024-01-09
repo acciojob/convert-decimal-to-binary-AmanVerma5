efef23c4c36e1cc9beeec14e972f3a282909f28b
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2024-01-09 15:32:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function decimalToBinary(num) {
 	
   // your code here
-	if(num===0) return 0;
+	if(num===0) return '0';
 	let binary='';
 	while(num>0){
 		binary=(num%2)+binary;
 		num=Math.floor(num/2);
 	}
 	return binary;
-
 }
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
